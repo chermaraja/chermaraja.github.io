99a74d43d43574387a27d5c7d55ac6d46fbd6a0c
chermaraja <chermaraja>
2020-10-22 09:41:41 +0530

    add app file

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
+
+
+//remove the last item
 var a =[1,2,3,4,5,6];
  a.pop();
- 
-
  console.log(a);
+  
+ // remove the first itme 
+ var a=[1,2,3,4,5,6];
+ a.shift();
+ console.log(a);
+
+ //add a value of last item
+ 
+ var a=[1,2,3,4,5];
+ a.push(8);
+ console.log(a);
+
+ //add a value of first item
+  var a=[1,2,3,4,5];
+  a.unshift(5);
+  console.log(a);
+
+  //splice
+  var a=[1,3,5,7];
+  s=a.splice(2,4,6);
+  console.log("splice;",a);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
